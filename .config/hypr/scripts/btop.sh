3c5d4c3bc3309e2139cca1f8e9c799b7cacf7f1a
lerealet <lerealet@gmail.com>
2023-07-15 00:31:49 +0200

    wip

--- .config/hypr/scripts/btop.sh
+++ .config/hypr/scripts/btop.sh
@@ -1,6 +1,6 @@
 kitty --class btop-net btop -p 1 &
 kitty --class btop-proc btop -p 2 &
-sleep 0.3
+sleep 0.5
 hyprctl --batch dispatch togglefloating 'btop-net' \;\
   dispatch resizewindowpixel exact 650 450,'btop-net' \; \
   dispatch movewindowpixel exact 200 500,'btop-net' \; \
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # dotfiles
 Some configs
 
-
-link the desired theme file (e.g. `Twilight.conf`) to .config/kitty/theme.conf
